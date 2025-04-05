1a4fe34430bd634a71f214a5ce37e10ba58e2db3
Chaoran Lu <andyluloveusc@gmail.com>
2025-04-04 18:47:00 -0700

    add 7-two-sum

---
+++ src/unit-test/7-two-sum.test.ts
@@ -0,0 +1,26 @@
+/**
+ * @description binary search test
+ * @author clu
+ */
+
+import { twoSum2 } from "../7-two-sum";
+
+describe('Two Sum', () => {
+    it('functional test', () => {
+        const arr = [1, 2, 4, 7, 11, 15];
+        const res = twoSum2(arr, 15);
+        expect(res).toEqual([4, 11]);
+    })
+
+    it('empty array', () => {
+        const res = twoSum2([], 100);
+        expect(res).toEqual([]);
+    })
+
+    it('result cannot be found', () => {
+        const arr = [1, 2, 4, 7, 11, 15];
+        const n = 100;
+        const res = twoSum2(arr, n);
+        expect(res).toEqual([]);
+    })
+})
