c8a98f8305744f0d3414e616352481c83d4bcde1
Chaoran Lu <andyluloveusc@gmail.com>
2025-07-10 16:20:10 -0400

    leetcode 14.Longest Common Prefix, implementation & unit-test

---
+++ src/200-easy/4-lc-longest-common-prefix.ts
@@ -0,0 +1,14 @@
+/**
+ * Write a function to find the longest common prefix string amongst an array of strings
+ * @param strs
+ */
+export function longestCommonPrefix(strs: string[]): string {
+    let prefix = strs[0];
+    for(let i = 1; i < strs.length; i++) {
+        while(!strs[i].startsWith(prefix)) {
+            prefix = prefix.slice(0, -1);
+        }
+        if (prefix === "") return "";
+    }
+    return prefix;
+}
---
+++ src/unit-test/200-easy/4-lc-longest-common-prefix.test.ts
@@ -0,0 +1,10 @@
+import { longestCommonPrefix } from "../../200-easy/4-lc-longest-common-prefix";
+
+describe("Write a function to find the longest common prefix string amongst an array of strings", ()=>{
+    it("input string array is flower, flow, flight", ()=>{
+        expect(longestCommonPrefix(["flower", "flow", "flight"])).toBe("fl");
+    });
+    it("input string array is dog, racecar, car", ()=>{
+        expect(longestCommonPrefix(["dog", "racecar", "car"])).toBe("");
+    });
+})
