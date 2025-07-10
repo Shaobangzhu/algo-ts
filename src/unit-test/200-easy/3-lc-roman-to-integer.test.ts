38be6b6250e2305085c3f6dda29a7766b0514654
Chaoran Lu <andyluloveusc@gmail.com>
2025-07-09 23:38:44 -0400

    leetcode 13.Roman to Integer, implementation & unit-test

---
+++ src/unit-test/200-easy/3-lc-roman-to-integer.test.ts
@@ -0,0 +1,13 @@
+import { romanToInt } from "../../200-easy/3-lc-roman-to-integer"
+
+describe("transform roman to interger number", ()=>{
+    it("input string is III", ()=>{
+        expect(romanToInt("III")).toBe(3);
+    });
+    it("input string is LVIII", ()=>{
+        expect(romanToInt("LVIII")).toBe(58);
+    });
+    it("input string is MCMXCIV", ()=>{
+        expect(romanToInt("MCMXCIV")).toBe(1994);
+    });
+})
