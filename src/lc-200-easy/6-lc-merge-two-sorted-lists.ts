4d31a50375a9fb10ac283b5ea5994d6dcb2944c8
Chaoran Lu <andyluloveusc@gmail.com>
2025-07-11 16:58:24 -0400

    leetcode 21.Merge Two Sorted Lists, implementation & unit-test

--- src/lc-200-easy/6-lc-merge-two-sorted-lists.ts
+++ src/lc-200-easy/6-lc-merge-two-sorted-lists.ts
@@ -2,11 +2,26 @@ import ListNode from "../utils/list-node";
 
 /**
  * 21. Merge Two Sorted Lists
- * Time Complexity:
- * Space Complexity:
+ * Time Complexity: O(n + m), where n and m are the length of the two lists - each node is processed once
+ * Space Complexity: O(1), rearranges pointers in-place, aside from the dummy node
  * @param list1
  * @param list2 
  */
-function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
+export function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
+    const dummy = new ListNode(0);
+    let current = dummy;
+
+    while(list1 && list2) {
+        if (list1.val < list2.val) {
+            current.next = list1;
+            list1 = list1.next;
+        } else {
+            current.next = list2;
+            list2 = list2.next;
+        }
+        current = current.next;
+    }
+    current.next = list1 !== null ? list1 : list2;
     
+    return dummy.next;
 }
---
+++ src/unit-test/lc-200-easy/6-lc-merge-two-sorted-lists.test.ts
@@ -0,0 +1,45 @@
+import { mergeTwoLists } from "../../lc-200-easy/6-lc-merge-two-sorted-lists";
+import ListNode from "../../utils/list-node";
+
+// Helper: Convert array to linked list
+function buildList(arr: number[]): ListNode | null {
+  const dummy = new ListNode(0);
+  let current = dummy;
+  for (const num of arr) {
+    current.next = new ListNode(num);
+    current = current.next;
+  }
+  return dummy.next;
+}
+
+// Helper: Convert linked list to array
+function listToArray(head: ListNode | null): number[] {
+    const result: number[] = [];
+    let current = head;
+    while (current) {
+        result.push(current.val);
+        current = current.next;
+    }
+    return result;
+}
+
+describe("21.Merge Two Sorted Lists", () => {
+  it("list1=[1,2,4], list2=[1,3,4]", () => {
+    const l1 = buildList([1, 2, 4]);
+    const l2 = buildList([1, 3, 4]);
+    const result = mergeTwoLists(l1, l2);
+    expect(listToArray(result)).toEqual([1,1,2,3,4,4]);
+  });
+  it("list1=[], list2=[]", () => {
+    const l1 = buildList([]);
+    const l2 = buildList([]);
+    const result = mergeTwoLists(l1, l2);
+    expect(listToArray(result)).toEqual([]);
+  });
+  it("list1=[], list2=[0]", () => {
+    const l1 = buildList([]);
+    const l2 = buildList([0]);
+    const result = mergeTwoLists(l1, l2);
+    expect(listToArray(result)).toEqual([0]);
+  });
+});
