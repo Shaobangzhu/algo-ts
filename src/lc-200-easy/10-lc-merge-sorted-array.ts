2abe7862203d23299953517bbafd13d13dc48a13
Chaoran Lu <andyluloveusc@gmail.com>
2025-07-14 11:03:57 -0700

    implement leetcode 88. Merge Sorted Array and its unit test

---
+++ src/lc-200-easy/10-lc-merge-sorted-array.ts
@@ -0,0 +1,25 @@
+/**
+ * 88. Merge Sorted Array
+ * Given two integer arrays nums1 and nums2, sorted in non-decreasing order. 
+ * The final sorted array should not be returned by the function, but instead be sorted inside the array nums1.
+ * @param nums1 
+ * @param m number of the elements in nums1
+ * @param nums2 
+ * @param n number of the elements in nums2
+ */
+export function merge(nums1: number[], m: number, nums2: number[], n: number): void {
+    let i = m - 1;
+    let j = n - 1;
+    let k = m + n - 1;
+
+    while(j >= 0) {
+        if (i >= 0 && nums1[i] > nums2[j]) {
+            nums1[k] = nums1[i];
+            i--; 
+        } else {
+            nums1[k] = nums2[j];
+            j--;
+        }
+        k--;
+    }
+}
